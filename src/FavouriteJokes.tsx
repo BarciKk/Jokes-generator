e5d431fc04f65b3aeb4310802905a1099c7cfc8c
BarciKk <kamil.bartuzel2@o2.pl>
2022-12-27 22:05:22 +0100

    First commit

---
+++ src/FavouriteJokes.tsx
@@ -0,0 +1,33 @@
+import { Joke } from "./Joke";
+import { useContext } from "react";
+import { JokesAndLikeHanlder } from "./App";
+import { JokeContext, Title } from "./styledComponents/StyledComponents";
+import { motion as m } from "framer-motion";
+
+export const FavouriteJokes = () => {
+  const context = useContext(JokesAndLikeHanlder);
+  return (
+    <m.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.9, ease: "backOut" }}
+      className="fav"
+    >
+      {context?.likeJoke.length === 0 && (
+        <Title>You don't have liked any jokes yet</Title>
+      )}
+      <JokeContext>
+        {context?.likeJoke.map(({ id, setup, delivery }) => (
+          <Joke
+            id={id}
+            key={id}
+            setup={setup}
+            delivery={delivery}
+            handleToggleLike={context.handleToggleLike}
+            likedJokes={context.likeJoke}
+          />
+        ))}
+      </JokeContext>
+    </m.div>
+  );
+};
---
+++ src/types.ts
@@ -0,0 +1,38 @@
+interface IJoke {
+  error: boolean;
+  category: string;
+  type: string;
+  setup?: string;
+  delivery?: string;
+  flags: {
+    nsfw: boolean;
+    religious: boolean;
+    political: boolean;
+    racist: boolean;
+    sexist: boolean;
+    explicit: boolean;
+  };
+  id: number;
+  safe: boolean;
+  lang: string;
+}
+
+export type HandleToggleLike = (
+  targetedJokeId: IJoke["id"],
+  isLiked: boolean
+) => void;
+
+interface dupa {
+  likedJoke: IJoke[];
+  handleToggleLike: HandleToggleLike;
+}
+interface DupaDupa {
+  joke: IJoke | undefined;
+  setJoke: React.Dispatch<React.SetStateAction<IJoke | undefined>>;
+}
+
+export type IJokeProps = Pick<IJoke, "id" | "setup" | "delivery"> & {
+  likedJokes: IJoke[];
+  handleToggleLike: HandleToggleLike;
+};
+export type { IJoke, dupa, DupaDupa };
